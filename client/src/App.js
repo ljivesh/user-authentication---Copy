ff3d6139b778f0665bfc2818c9d940ab67bc7657
Menma <96004270+ljivesh@users.noreply.github.com>
2023-12-04 23:09:26 +0530

    passed cart prop and addItemToCart prop

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,20 @@ import ProductList from "./Components/ProductList";
 // import { useState } from "react";
 
 function App() {
-  // const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  //function to be passed as prop
+  const addItemToCart = (item)=> {
+    setCart(prev=> [...prev, item]);
+  };
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route path="/" element={<ProductList addItemToCart={addItemToCart} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart cart={cart} />} />
 
       </Routes>
     </div>
